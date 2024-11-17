dc1c4abdb265862618a616eb059de8b266953935
UmaMahesh630 <uma.rao05041997@gmail.com>
2024-11-16 19:46:58 -0800

    IsPalindrome

---
+++ IsPalindrome.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string str = s.ToLower();
+        int left = 0, right = str.Length-1;
+
+        while(left <= right) {
+            //if any charater is other than letter or digit while moving from left
+            if(!char.IsLetterOrDigit(str[left])){
+                left++;
+            }
+            //if any charater is other than letter or digit while moving from right
+            else if(!char.IsLetterOrDigit(str[right])){
+                right--;
+            }
+            else
+            {
+                if(str[left] != str[right]){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
